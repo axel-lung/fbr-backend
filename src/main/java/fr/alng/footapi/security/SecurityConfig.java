8b93aed4fd0496d30074a9a831e8e2cb70032088
axel-lung <axel.lung@outlook.fr>
2022-09-14 19:48:36 +0200

    csrf disable

--- src/main/java/fr/alng/footapi/security/SecurityConfig.java
+++ src/main/java/fr/alng/footapi/security/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 
 @Configuration @EnableWebSecurity @RequiredArgsConstructor
@@ -35,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        http.csrf().disable();
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/api/login/**", "/api/user/save/**").permitAll();
