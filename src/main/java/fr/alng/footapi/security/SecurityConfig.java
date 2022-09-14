0906987b350afdf2dcac0d23d8257e262694d643
axel-lung <axel.lung@outlook.fr>
2022-09-14 19:19:54 +0200

    corrections

--- src/main/java/fr/alng/footapi/security/SecurityConfig.java
+++ src/main/java/fr/alng/footapi/security/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 
 
 @Configuration @EnableWebSecurity @RequiredArgsConstructor
